e0a24593624f5d2a84b8eb92223381a72cc77310
Valery Baranov <v.baranov@integer-soft.ru>
2023-03-31 23:31:37 +0400

    add docker build frontend ci

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,12 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-
-import rose from './pngwing.com.png';
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <p>💞 &nbsp; 💞 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 💞 &nbsp; 💞</p>
-        <p>💞 С Днём Рождения, 💞</p>
-        <p>💞 солнышко!!! 💞</p>
-        <p>💞</p>
-
-        <img src={rose} className="rose"/>
-        <div className="ps">Любимой от любимого /\/</div>
+        To be done...
       </div>
-      {/*<header className="App-header">*/}
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        {/*<p>*/}
-        {/*  Edit <code>src/App.tsx</code> and save to reload.*/}
-        {/*</p>*/}
-        {/*<a*/}
-        {/*  className="App-link"*/}
-        {/*  href="https://reactjs.org"*/}
-        {/*  target="_blank"*/}
-        {/*  rel="noopener noreferrer"*/}
-        {/*>*/}
-        {/*  Learn React*/}
-        {/*</a>*/}
-      {/*</header>*/}
     </div>
   );
 }
