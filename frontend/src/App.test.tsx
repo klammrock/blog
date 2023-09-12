5197285acc35165f312990e1f04891145d063825
klammrock <klammrock@gmail.com>
2023-09-13 00:14:29 +0400

    fix test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/To be done.../i);
+  const linkElement = screen.getByText(/Content/i);
   expect(linkElement).toBeInTheDocument();
 });
